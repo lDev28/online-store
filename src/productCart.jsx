3747655528991c954dd7a164968eaf1365f5781b
lDev28 <arslan_28.03@inbox.ru>
2023-09-10 20:46:25 +0500

    debugged

--- src/productCart.jsx
+++ src/productCart.jsx
@@ -5,12 +5,10 @@ import AUTO_PARFUMES from './data/categories/auto_parfumes/AUTO_PARFUMES.JS'
 import img from './data/categories/auto_parfumes/ocean.jpg'
 
 const ProductCart = ({ product, children }) => {
-	const img = require('./data/categories/auto_parfumes/ocean.jpg')
 	return (
 		<div className={styles.Product__cart}>
 			<h3>{product.name}</h3>
 			<img src={img} alt='img' />
-			{/* <img src='./data/categories/auto_parfumes/ocean.jpg' alt='img' /> */}
 			<p>Категория: {product.category}</p>
 			<p>Цена: {product.price}</p> {children}
 		</div>
