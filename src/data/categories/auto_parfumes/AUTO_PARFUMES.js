0b4226c498f74ed075bcd761f12f105761a0ebbd
lDev28 <arslan_28.03@inbox.ru>
2023-09-10 21:17:38 +0500

    debugged again

--- src/data/categories/auto_parfumes/AUTO_PARFUMES.js
+++ src/data/categories/auto_parfumes/AUTO_PARFUMES.js
@@ -2,8 +2,8 @@ const AUTO_PARFUMES = [
 	{
 		id: 1,
 		name: 'авто парфюм ОКЕАН 10мл',
-		price: 25,
-		img: './data/categories/auto_parfumes/ocean.jpg'
+		price: 25
+		// img: './data/categories/auto_parfumes/ocean.jpg'
 	}
 ]
 export default AUTO_PARFUMES
--- src/productCart.jsx
+++ src/productCart.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import styles from './products.module.scss'
-import img from './data/categories/auto_parfumes/ocean.jpg'
 
 const ProductCart = ({ product, children }) => {
 	return (
 		<div className={styles.Product__cart}>
 			<h3>{product.name}</h3>
-			<img src={img} alt='img' />
 			<p>Категория: {product.category}</p>
 			<p>Цена: {product.price}</p> {children}
 		</div>
